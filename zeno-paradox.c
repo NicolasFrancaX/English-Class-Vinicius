68321415332b854bd7be265e754c578c84abeed8
NicolasFrancaX <nicolasfranca9@gmail.com>
2018-05-23 22:23:53 -0300

    Commit some test cases of zeno-paradox.

---
+++ zeno-paradox.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// I'll comment all the code right here and put in gist.github, ok?
+//
+// You can talk to me in chat at Hangouts. I'll listenig you.
+//
+
+// The Zeno's Paradox work like this:
+//
+// 1 = 1/2 + 1/4 + 1/8 + 1/16 + ...
+//
+// I'll show you this.
+
+///float zeno-paradox(float number);
+
+float zeno_paradox(float number) {
+    return 1;
+}
+
+int main() {
+
+    // So, lets start with a test:
+    printf("1 = %.0f\n", zeno_paradox(1));
+
+
+    // You can answer me in Hangouts. What you think that will happen right now?
+
+    // This is TDD. And we are using baby-steps.
+    
+    // Let's see if our first test will work.
+    
+    // The code worked. But and with the number two?
+    
+    printf("2 = %.0f\n", zeno_paradox(2));
+
+    // Didn't work.
+    
+    // Now it's your time. We will change the pilot of our PP (Pair Programming). Ok?
+    
+    // I'll commit this code. Ok? Answer me at Hangouts.
+
+}
