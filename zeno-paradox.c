55e8128ad3b6065d6adfb28e58527766f9c7d2a4
vinicius <vinicius@vizir.com.br>
2018-05-30 21:24:06 -0300

    Make for to zeno_paradox function

--- zeno-paradox.c
+++ zeno-paradox.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 // I'll comment all the code right here and put in gist.github, ok?
 //
@@ -14,6 +15,10 @@
 ///float zeno-paradox(float number);
 
 float zeno_paradox(float number) {
+    for(int i = 0; i < 1000; i++) {
+        pow(2,i);
+    }
+
     return 1;
 }
 
@@ -26,17 +31,18 @@ int main() {
     // You can answer me in Hangouts. What you think that will happen right now?
 
     // This is TDD. And we are using baby-steps.
-    
+
     // Let's see if our first test will work.
-    
+
     // The code worked. But and with the number two?
-    
+
     printf("2 = %.0f\n", zeno_paradox(2));
 
     // Didn't work.
-    
+
     // Now it's your time. We will change the pilot of our PP (Pair Programming). Ok?
-    
+
     // I'll commit this code. Ok? Answer me at Hangouts.
 
+    // 
 }
